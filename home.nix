a75341b002a08d142f1b208db02810cdeb8a5a20
qwaxgo <qwaxgo@gmail.com>
2025-04-29 18:41:12 +0900

    wallpaper and package added

--- home.nix
+++ home.nix
@@ -5,4 +5,19 @@
     stateVersion = "24.11";
   };
   programs.home-manager.enable = true;
+
+  home.file = {
+    "wallpaper.png" = {
+      target = "Wallpaper/wallpaper.png";
+      source = ./wallpaper.png;
+    };
+  };
+
+  home.packages = with pkgs; [
+    bat
+    bottom
+    eza
+    httpie
+    ripgrep
+  ];
 }
