6e8ab7eec546a52e309c511195ec573c162dcb65
qwaxgo <qwaxgo@gmail.com>
2025-05-04 20:44:38 +0900

    fix init.lua

--- init.lua
+++ init.lua
@@ -1,10 +1,10 @@
 local opt = vim.opt
 
 -- マウス有効化
-opt.mouse = 'a'
+opt.mouse = "a"
 opt.title = true
 -- 全角文字表示設定
-opt.ambiwidth = 'double'
+opt.ambiwidth = "double"
 
 opt.swapfile = false
 opt.backup = false
@@ -13,26 +13,25 @@ opt.clipboard:append({unnamedplus = true})
 
 opt.number = true
 opt.list = true
-opt.listchars = 'tab:>-', 'trail:*', 'nbsp:+'
+opt.listchars = { tab = ">-", trail = "*", nbsp = "+" }
 opt.smartindent = true
 opt.visualbell = true
 
 opt.showmatch = true
 
 opt.expandtab = true
-opt.tabstop = 4
-opt.shiftwidth = 4
+opt.tabstop = 2
+opt.shiftwidth = 2
 
 opt.ignorecase = true
 opt.smartcase = true
 opt.wrapscan = true
 
-opt.whichwrap = 'b', 's', 'h', 'l', '<', '>', '[', ']'
-opt.backspace = 'start', 'eol', 'indent'
--- Windows環境なのでdosが最初
-opt.fileformats = 'unix', 'dos', 'mac'
+opt.whichwrap = { "b", "s", "h", "l", "<", ">", "[", "]" }
+opt.backspace = { "indent", "eol", "start" }
+opt.fileformats = { "unix", "dos", "mac" }
 
-opt.helplang = 'ja', 'en'
+opt.helplang = { "ja", "en" }
 
 opt.updatetime = 300
 
